bd07ee95297efa9c17f3342d63dff71b660be9d9
mrcuong84 <nguyen.manh.cuong@vareal.vn>
2019-11-14 13:14:51 +0700

    demo-auto-test

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.3'
 
 gem 'selenium-webdriver', '~> 3.142', '>= 3.142.6'
+gem 'pry'
--- demo.rb
+++ demo.rb
@@ -1,4 +1,5 @@
 require "selenium-webdriver"
+require "pry"
 
 #TODO Read users from csv file
 [
@@ -18,23 +19,50 @@ require "selenium-webdriver"
   # options.add_argument('--headless')
 
   driver = Selenium::WebDriver.for :chrome, options: options, http_client: client
-  driver.navigate.to "https://pc-monka.dev.nnn.ed.nico/"
+  driver.navigate.to "https://dev.nnn.ed.nico/"
 
-  btn_login_niconico = driver.find_element(link_text: "ログイン")
-  if btn_login_niconico.displayed?
-    puts "Start login niconico..."
-    btn_login_niconico.click
-  end
+  btn1_login_niconico = driver.find_element(link_text: "ログイン")
+  btn1_login_niconico.click
 
-  texb_login_email = driver.find_element(id: "input__mailtel")
-  texb_login_password = driver.find_element(id: "input__password")
-  btn_login_submit = driver.find_element(id: "login__submit")
+  text_login_email = driver.find_element(id: "input__mailtel")
+  text_login_email.send_keys user[:email]
+  text_login_password = driver.find_element(id: "input__password")
+  text_login_password.send_keys user[:password]
+  btn_auth_submit = driver.find_element(id: "login__submit")
+  btn_auth_submit.click
 
-  texb_login_email.send_keys user[:email]
-  texb_login_password.send_keys user[:password]
-  btn_login_submit.click
+  driver.find_element(class: "okay").click()
 
-  #TODO: update user information
+  btn_choice1 = driver.find_element(link_text: "はじめる")
+  btn_choice1.click
+
+  btn_choice2 = driver.find_element(link_text: "同意して設定する")
+  btn_choice2.click
+
+  btn_choice3 = driver.find_element(name: "commit")
+  btn_choice3.click
+
+  driver.find_element(name:"year").send_keys('1988')
+  driver.find_element(name:"month").send_keys('10')
+  driver.find_element(name:"day").send_keys('9')
+
+  driver.find_element(name:'profile[last_name]').send_keys('ます')
+  driver.find_element(name:'profile[first_name]').send_keys('ます')
+  driver.find_element(name:'profile[last_name_kana]').send_keys('ます')
+  driver.find_element(name:'profile[first_name_kana]').send_keys('ます')
+  driver.find_element(name:'profile[postal_code]').send_keys('1640013')
+  driver.find_element(name:'profile[phone_number]').send_keys('123456789')
+
+  btn_choice4 = driver.find_element(name: "commit")
+  btn_choice4.click
+
+  driver.find_element(name:'daigaku_juken[job_or_grade_id]').send_keys('一般社会人')
+
+  btn_choice5 = driver.find_element(name: "commit")
+  btn_choice5.click
+
+  btn_choice6 = driver.find_element(link_text: "N予備校ホームへ")
+  btn_choice6.click
 
   driver.quit
 end
